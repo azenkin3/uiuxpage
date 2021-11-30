57e32320cc84a4be9df315f5d931e345347a4309
kwonch <rnjsskfen153@naver.com>
2021-11-30 17:45:52 +0900

    12

---
+++ js/nav.js
@@ -0,0 +1,18 @@
+$(function(){
+$('.navbar__toggleBtn').click(function(){
+  $('.navbar__menu').slideToggle();
+});
+});
+
+$(window).resize(function(){
+  if(window.innerWidth>700){
+    $('.navbar__menu').show();
+    $('.navbar__toogleBtn').hide();
+  }
+});
+$(window).resize(function(){
+  if(window.innerWidth<700){
+    $('.navbar__menu').hide();
+}  
+ 
+});
